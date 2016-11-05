6a2e9ae18097610d934be480ffefc5fd85f438f0
Arnold Antoo <antooarnold404@gmail.com>
2016-11-05 14:07:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
-}
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -51,7 +51,7 @@ function createTemplate (data){
             var heading=data.heading;
             var content=data.content;
             
-            var htmlTemplate= `
+            var htmlTemplate= 
                        <html>
                 
                 <head>
