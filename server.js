2bb7d827fe28736ec19894d725220e6bed3f50bb
Arnold Antoo <antooarnold404@gmail.com>
2016-11-18 11:51:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
 var crypto = require('crypto');
-var bodyParser = require('body-parser');
+var bodyParser = require('bodyParser');
 
 app.use(morgan('combined'));
 
