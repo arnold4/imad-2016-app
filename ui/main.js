e85abfeade30a7ee7a8e6b18bb8cb6e579713060
Arnold Antoo <antooarnold404@gmail.com>
2016-11-19 11:06:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,21 @@
     // Submit username/password to login
     var submit = document.getElementById('submit_btn');
     submit.onclick = function () {
+        
         // Create a request object
         var request = new XMLHttpRequest();
         
-        // Capture the response and store it in a variable
+    // Capture the response and store it in a variable
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  submit.value = 'Sucess!';
+                 alert('Logged In Successfully ');
               } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
+                  alert('Invalid Credentials ');
               } else if (request.status === 500) {
                   alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              } else {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              }
-              loadLogin();
+              } 
           }  
           // Not done yet
         };
@@ -32,8 +28,7 @@
         request.open('POST', 'http://arnold4.imad.hasura-app.io/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
-      //  submit.value = 'Logging in...';
-        
+  
     };
   
   
