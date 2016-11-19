a15aa4046ff5cb654fb0e2835624b3f2addf1f74
Arnold Antoo <antooarnold404@gmail.com>
2016-11-19 11:19:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,9 +10,9 @@
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                 alert('Logged In Successfully ');
+                 alert('Logged In Successfully');
               } else if (request.status === 403) {
-                  alert('Invalid Credentials ');
+                  alert('Invalid Credentials');
               } else if (request.status === 500) {
                   alert('Something went wrong on the server');
               } 
@@ -32,36 +32,4 @@
     };
   
   
-  /*  
-    var register = document.getElementById('register_btn');
-    register.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  alert('User created successfully');
-                  register.value = 'Registered!';
-              } else {
-                  alert('Could not register the user');
-                  register.value = 'Register';
-              }
-          }
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/create-user', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        register.value = 'Registering...';
-    
-};
-
-*/
+  
