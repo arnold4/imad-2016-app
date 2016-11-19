f146b3aabc2499c18748298916438f05cefe45ef
Arnold Antoo <antooarnold404@gmail.com>
2016-11-19 11:35:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -200,7 +200,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
                             if (err) {
                                 res.status(500).send(err.toString());
                             } else {
-                                res.status(200).send('Comment inserted!')
+                                res.status(200).send('Comment inserted!');
                             }
                         });
                 }
@@ -226,6 +226,7 @@ app.get('/articles/:articleName', function (req, res) {
     }
   });
 });
+
 
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
